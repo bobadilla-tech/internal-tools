4b167e6cfe91c54e18d0f7fd02a11cc5efb033b7
UltiRequiem <eliaz.bobadilladev@gmail.com>
2026-04-17 22:46:24 -0500

    tweaks

--- scripts/patch-plane-settings.py
+++ scripts/patch-plane-settings.py
@@ -10,8 +10,8 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST", "localhost")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 25))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "False").lower() in ("true", "1")
-EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "False").lower() in ("true", "1")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "0") == "1"
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "0") == "1"
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "noreply@localhost")
 SERVER_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "noreply@localhost")
 """
